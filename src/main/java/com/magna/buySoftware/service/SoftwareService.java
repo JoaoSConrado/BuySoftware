85ae7aeead1787e2acd1c603735f5bcbc5e997d4
João Conrado <JoaoSConrado@gmail.com>
2022-11-21 17:40:43 -0300

    Implementação de Loggers

--- src/main/java/com/magna/buySoftware/service/SoftwareService.java
+++ src/main/java/com/magna/buySoftware/service/SoftwareService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +20,8 @@ import com.magna.buySoftware.vo.SoftwareVO;
 
 @Service
 public class SoftwareService {
+
+	private static Logger logger = LoggerFactory.getLogger(UsuarioService.class);
 
 	@Autowired
 	SoftwareRepository softwareRepository;
@@ -50,17 +54,21 @@ public class SoftwareService {
 	}
 
 	public Page<SoftwareVO> buscarTodosSoftwares(Pageable pageable) {
+		logger.info("LISTANDO TODOS OS SOFTWARES CADASTRADOS");
 		return softwareRepository.findAll(pageable).map(this::converterSoftwareEntityParaSoftwareVO);
 	}
 
 	public SoftwareVO buscarSoftwarePorId(Long id) {
+		logger.info("BUSCANDO SOFTWARE POR ID");
 		Optional<SoftwareEntity> software = Optional.of(softwareRepository.findById(id)
 				.orElseThrow(() -> new DadosNaoEncontradosException("Software não foi encontrado!")));
 		SoftwareEntity softwareEntity = software.get();
+		logger.info("SOFTWARE COM O ID: {}! ENCONTRADO COM SUCESSO!", id);
 		return this.converterSoftwareEntityParaSoftwareVO(softwareEntity);
 	}
 
 	public void salvarSoftware(Long id, SoftwareVO softwareVO) {
+		logger.info("CADASTRANDO SOFTWARE");
 		Optional<UsuarioEntity> usuario = Optional.of(usuarioRepository.findById(id)
 				.orElseThrow(() -> new DadosNaoEncontradosException("O Usuario não foi encontrado!")));
 		SoftwareVO software = new SoftwareVO();
@@ -70,15 +78,19 @@ public class SoftwareService {
 		software.setDataEntrega(softwareVO.getDataEntrega());
 		software.setUsuarioEntity(usuario.get());
 		software.setDataPedido(LocalDateTime.now(ZoneId.of("America/Sao_Paulo")));
+		logger.info("O SOFTWARE DO USUARIO: {}! FOI CADASTRADO COM SUCESSO!", usuario.get().getNome());
 		softwareRepository.save(converterSoftwareVOParaSoftwareEntity(software));
 	}
 
 	public void deletarSoftware(Long id) {
+		logger.info("DELETANDO SOFTWARE");
 		buscarSoftwarePorId(id);
 		softwareRepository.deleteById(id);
+		logger.info("SOFTWARE COM ID: {}! DELETADO COM SUCESSO!", id);
 	}
 
 	public SoftwareVO atualizarSoftware(Long id, SoftwareVO software) {
+		logger.info("ATUALIZANDO SOFTWARE");
 		Optional<SoftwareEntity> softwareEntity = Optional.of(softwareRepository.findById(id)
 				.orElseThrow(() -> new DadosNaoEncontradosException("Software não foi encontrado!")));
 		SoftwareEntity entity = softwareEntity.get();
@@ -87,6 +99,7 @@ public class SoftwareService {
 		entity.setDesenvolvedor(software.getDesenvolvedor());
 		entity.setDataEntrega(software.getDataEntrega());
 		SoftwareEntity atualizarSoftware = softwareRepository.save(softwareEntity.get());
+		logger.info("SOFTWARE ATUALIZADO COM SUCESSO!");
 		return converterSoftwareEntityParaSoftwareVO(atualizarSoftware);
 	}
 }
--- src/main/java/com/magna/buySoftware/service/UsuarioService.java
+++ src/main/java/com/magna/buySoftware/service/UsuarioService.java
@@ -2,6 +2,8 @@ package com.magna.buySoftware.service;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +17,8 @@ import com.magna.buySoftware.vo.UsuarioVO;
 
 @Service
 public class UsuarioService {
+
+	private static Logger logger = LoggerFactory.getLogger(UsuarioService.class);
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
@@ -42,29 +46,38 @@ public class UsuarioService {
 	}
 
 	public Page<UsuarioVO> buscarTodosUsuarios(Pageable pageable) {
+		logger.info("LISTANDO TODOS OS USUARIOS CADASTRADOS");
 		return usuarioRepository.findAll(pageable).map(this::converterUsuarioEntityParaUsuarioVO);
 	}
 
 	public UsuarioVO buscarUsuarioPorId(Long id) {
+		logger.info("BUSCANDO USUARIO POR ID");
 		Optional<UsuarioEntity> usuario = Optional.of(usuarioRepository.findById(id)
 				.orElseThrow(() -> new DadosNaoEncontradosException("O Usuario não foi encontrado!")));
 		UsuarioEntity usuarioEntity = usuario.get();
+		logger.info("USUARIO COM O ID: {}! ENCONTRADO COM SUCESSO", usuario.get().getId());
 		return this.converterUsuarioEntityParaUsuarioVO(usuarioEntity);
 	}
 
 	public UsuarioVO salvarUsuario(UsuarioVO usuarioVO) {
+		logger.info("CADASTRANDO USUÁRIO");
+		logger.info("VERIFICANDO SE CPF & EMAIL ESTÃO CADASTRADOS");
 		verificaCPF(usuarioVO);
 		verificaEmail(usuarioVO);
 		UsuarioEntity salvarUsuario = usuarioRepository.save(converterUsuarioVOParaUsuarioEntity(usuarioVO));
+		logger.info("USUARIO: {}! CADASTRADO COM SUCESSO!", usuarioVO.getNome());
 		return this.converterUsuarioEntityParaUsuarioVO(salvarUsuario);
 	}
 
 	public void deletarUsuario(Long id) {
+		logger.info("DELETANDO USUÁRIO");
 		buscarUsuarioPorId(id);
 		usuarioRepository.deleteById(id);
+		logger.info("USUÁRIO COM ID: {}! DELETADO COM SUCESSO!", id);
 	}
 
 	public UsuarioVO atualizarUsuario(Long id, UsuarioVO usuario) {
+		logger.info("ATUALIZANDO USUÁRIO");
 		Optional<UsuarioEntity> usuarioEntity = Optional.of(usuarioRepository.findById(id)
 				.orElseThrow(() -> new DadosNaoEncontradosException("O Usuario não foi encontrado!")));
 		UsuarioEntity entity = usuarioEntity.get();
@@ -73,17 +86,20 @@ public class UsuarioService {
 		entity.setEmail(usuario.getEmail());
 		entity.setSenha(usuario.getSenha());
 		UsuarioEntity atualizarUsuario = usuarioRepository.save(usuarioEntity.get());
+		logger.info("USUÁRIO: {}! ATUALIZADO COM SUCESSO!", usuario.getNome());
 		return converterUsuarioEntityParaUsuarioVO(atualizarUsuario);
 	}
 
 	private void verificaCPF(UsuarioVO usuario) {
 		if (usuarioRepository.findByCpf(usuario.getCpf()).isPresent()) {
+			logger.info("ERRO NO CADASTRO: CPF JÁ CADASTRADO!");
 			throw new JaFoiCadastradoException("Já existe um Usuario cadastrado com esse CPF!");
 		}
 	}
 
 	private void verificaEmail(UsuarioVO usuario) {
 		if (usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
+			logger.info("ERRO NO CADASTRO: EMAIL JÁ CADASTRADO!");
 			throw new JaFoiCadastradoException("Já existe um Usuario cadastrado com esse EMAIL!");
 		}
 	}
